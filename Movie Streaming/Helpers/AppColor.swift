a893b493cdc5f74b76bf5aebc316cc1751f178fa
LTT <tuanlt.ptit@gmail.com>
2022-07-30 23:34:40 +0700

    create app color

---
+++ Movie Streaming/Helpers/AppColor.swift
@@ -0,0 +1,13 @@
+//
+//  AppColor.swift
+//  Movie Streaming
+//
+//  Created by LTT on 30/07/2022.
+//
+
+import Foundation
+import UIKit
+
+struct AppColor {
+    static let cFFFFFF_181A20 = UIColor(named: "FFFFFF-181A20")
+}
